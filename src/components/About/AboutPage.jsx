462e15bc54b334b25e1ad2058c11f114e6abb66e
dollyllama <edolly2@protonmail.com>
2023-03-05 20:14:54 -0600

    ""

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -4,6 +4,9 @@ const AboutPage = () => {
   return (
     <MainLayout>
       <PageTitle text='About Us' />
+      <h2>Our Story</h2>
+      <h2>Meet The Team</h2>
+      <h2>Mission Statement</h2>
     </MainLayout>
   );
 };
